8064cb297b682fc4631ad26811c84c92d180de5d
JunhyeokKim <junhyeok.kim0506@gmail.com>
2017-06-22 12:35:08 +0900

    파일 업로드

--- src/springweb/f01_file/A01_FileCtrl.java
+++ src/springweb/f01_file/A01_FileCtrl.java
@@ -1,8 +1,9 @@
 package springweb.f01_file;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,23 +24,55 @@ public class A01_FileCtrl {
 	public String upload(@RequestParam(value = "report") MultipartFile report) {
 		System.out.println("클라이언트로부터 업로드 요청된 파일: " + report.getOriginalFilename());
 		// service단 처리..
-
+		uploadProc(report);
+		System.out.println("hihi");
 		return "f01_file/a01_uploadForm";
 	}
+	// <input name="fname"/>
 
 	// 파일 이름과 넘겨온 스트림 형태의 객체를 server의 특정한 위치에 저장
-	private void uploadProd(String name, MultipartFile report) {
+	private void uploadProc(MultipartFile report) {
+		String name = report.getOriginalFilename();
 		// eclipse 임시 공간
 		// 실제 파일을 로딩
-		String path01 = "";
-		String path02= "";
+		String path01 = "C:/junhyeok/a01_prog/workspace/.metadata/.plugins/org.eclipse.wst.server.core/tmp1/webapps/springweb/z02_upload/";
+		String path02 = "C:/junhyeok/a01_prog/workspace/springweb/WebContent/z02_upload/";
+		// 해당 경로로 파일을 할당
+		String file01 = path01 + name;
+		String file02 = path02 + name;
+		// MultipartFile ==> File로 전환
+		File f1 = new File(file01);
+		File f2 = new File(file02);
+		MultipartFile mf = report;
 		try {
-			InputStream inputStream = report.getInputStream();
+			mf.transferTo(f1);
+			copy(f1,f2);
+		} catch (IllegalStateException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
 	}
+	
+	public void downloadProc(){
+		
+	}
+
+	// eclipse 임시 위치에 있는 파일을 사용할 위치로 copy 처리
+		private void copy(File org, File tgt) throws IOException {
+			FileInputStream in = new FileInputStream(org);
+			FileOutputStream out = new FileOutputStream(tgt);
+
+			byte[] buff = new byte[1024 * 8];
+			int nRead;
+			while ((nRead = in.read(buff)) != -1) {
+				out.write(buff, 0, nRead);
+			}
+			in.close();
+			out.close();
+
+		}
 
 }
